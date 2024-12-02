1684969952243aa1956a7a9a8b1c2961e24bbb44
Ankur <anx450z@gmail.com>
2024-12-03 00:03:36 +0530

    add multiple number summing tests

--- calculator_spec.rb
+++ calculator_spec.rb
@@ -16,5 +16,21 @@ RSpec.describe StringCalculator do
     it 'should return single number if there is a comma' do
       expect(calculator.add('2,')).to eq(2)
     end
+
+    it 'should return sum of numbers if there are multiple numbers' do
+      expect(calculator.add('1,2')).to eq(3)
+    end
+
+    it 'should return sum of numbers if there are multiple numbers and commas' do
+      expect(calculator.add('1,4,')).to eq(5)
+    end
+
+    it 'should handle any number of integers' do
+      expect(calculator.add('1,2,3,4,5,6,7,8,9,10')).to eq(55)
+    end
+
+    it 'should handle any number of integers with commas' do
+      expect(calculator.add('1,2,3,4,5,6,7,8,9,10,')).to eq(55)
+    end
   end
 end
