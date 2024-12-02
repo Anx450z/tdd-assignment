c68d653487034f776379b3c6d109de3e7b43f5a8
Ankur <anx450z@gmail.com>
2024-12-02 23:55:04 +0530

    add second test

--- calculator_spec.rb
+++ calculator_spec.rb
@@ -5,8 +5,12 @@ RSpec.describe StringCalculator do
   let(:calculator) { StringCalculator.new }
 
   describe 'add' do
-    it 'returns 0 if string is empty' do 
+    it 'should return 0 if string is empty' do 
       expect(calculator.add('')).to eq(0)
+    end
+
+    it 'should return single number if string is a number' do
+      expect(calculator.add('1')).to eq(1)
     end
   end
 end
